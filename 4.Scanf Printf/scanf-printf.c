8a1765d4b8461f03c1abffdf2c78cdea59b6d39f
alberto.sanchez <alberto.sanchez.delalamo@gmail.com>
2023-02-02 16:26:57 +0100

    exercise4

--- 4.Scanf Printf/scanf-printf.c
+++ 4.Scanf Printf/scanf-printf.c
@@ -4,5 +4,13 @@
 int main () {
     int int_small;
     long int int_big;
-    double double_small;
+    float float1;
+    
+    printf ("; %ld;  ; %4ld;  ; %-4ld;  ; %6.4ld;  ; %2d; \n", int_small, int_small, int_small, int_small, 111);
+	printf ("; %f;  ; %9f;  ; %-9f;  ; %9.4f;  ; %2f;  \n", float1, float1, float1, float1, 111);
+	printf ("; %c;  ; %s;  ; %7s;  ; %-7s;  ; %7.4s; \n", 'a', "uno", "dos", "tres", "cuatro");
+
+    printf("Write to integers separated by a ',' \n");
+    scanf("%d; %ld", &int_small, &int_big);
+    printf("; %d; ; %ld;", int_small, int_big);
 }

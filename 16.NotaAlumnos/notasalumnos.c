8e92a6ffaabafa9af1d9751ad8f6c22b42a09a12
alberto.sanchez <alberto.sanchez.delalamo@gmail.com>
2023-02-16 17:12:15 +0100

    o

--- 16.NotaAlumnos/notasalumnos.c
+++ 16.NotaAlumnos/notasalumnos.c
@@ -2,8 +2,48 @@
 #include <stdlib.h>
 
 
+typedef struct 
+{
+    char *name;
+    float *grades;
+} notasalumno;
+
+float request_notes();
+
 int main() {
+    int students, i;
+    float grades[3], *grades;
+    char *name;
+
+    printf("Introduce the number of students: ");
+    scanf("%i", &students);
+    printf("The number of students is %i", students);
+
+    for (i = 0; i < students; i++)
+    {
+        notasalumno * alumno;
+        notasalumno * alumno = (notasalumno*)malloc(sizeof(notasalumno));
+        
+        printf("Introduce the name of student %i: ", i);
+        scanf("%s", &name);
+        alumno -> name = &name;
+        grades[i] = request_notes();
+    }
     
 
+
     return 0;
+}
+
+float request_notes() {
+    float notes[3]; 
+    int i;
+
+    for (i = 0; i < 3; i++)
+    {
+        printf("Introduce a mark: ");
+        scanf("%f", &notes[i]);
+    }
+    
+    return notes;
 }
